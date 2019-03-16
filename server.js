1f4e812d51475900c26ccafdce1c64fe03bcbdb5
Cristiano Santos <cristianorsantos91@gmail.com>
2019-03-16 16:53:05 +0000

    Corrections

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/tour-of-heroes-working'));
+app.use(express.static(__dirname + '/dist/tour-of-heroes-aubilokos'));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/tour-of-heroes-working/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/tour-of-heroes-aubilokos/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
